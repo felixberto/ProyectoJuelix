1df8198eb3639155ea046dcf20959f3ce5a160d1
fisi <fisi@LAB03-17>
2013-09-27 17:24:13 -0500

    Este es mi primer commit soy juelix de la selva =D

---
+++ SoyLaComadreja/src/soylacomadreja/SoyLaComadreja.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soylacomadreja;
+
+/**
+ *
+ * @author fisi
+ */
+public class SoyLaComadreja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a =0;
+        System.out.println("Soy la comadreja xD: Valor entre 1 a 10");
+        a=(int)Math.random()*10+1;
+    }
+}
